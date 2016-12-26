b74cde2e90a00cd15502d757d5797e84d7ecdc6f
Anki25 <mishrankita25@yahoo.com>
2016-12-26 09:25:52 +0530

    back end commit

---
+++ ToDoBackEnd/src/main/java/com/employer/dao/TasksDAO.java
@@ -0,0 +1,24 @@
+package com.employer.dao;
+
+import java.util.List;
+
+import com.employer.model.Tasks;
+
+public interface TasksDAO {
+	
+    public boolean save(Tasks tasks); 
+	
+	public boolean update(Tasks tasks);
+	
+	public boolean delete(Tasks tasks);
+	
+	public Tasks get(int task_id);
+	
+	public List<Tasks> getByUserInfo(int user_id);
+	
+	public List<Tasks> list();
+	
+	public List<Tasks> taskOfUser(int user_id);
+
+
+}
